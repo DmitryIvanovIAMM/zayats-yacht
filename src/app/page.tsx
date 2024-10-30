21c99ee96309953d0e9ee5ec8c57da4afc638c2b
Zayats <dmitry.ivanov.iamm@gmail.com>
2024-10-30 11:04:20 +0200

    ZY-11 1. Correct font usage across the application 2. Correct common layout 3. Add AboutUs component without image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import styles from './page.module.css';
 import CopyrightFooter from '@/components/CopyrightFooter';
 import Navbar from '@/components/Navbar/Navbar';
 import ContactUs from '@/components/ContactUs/ContactUs';
+import AboutUs from '@/components/AboutUs/AboutUs';
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Navbar isAuthenticated={false} />
       <div className={styles.main}>
+        <AboutUs />
         <ContactUs />
       </div>
       <CopyrightFooter />

1d5cb3993f8b40f3366948ede1ae8ca52a33310f
Zayats <dmitry.ivanov.iamm@gmail.com>
2024-11-02 07:32:06 +0200

    ZY-14 Add loading: 'lazy' for each item for carousel

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -16,43 +16,50 @@ const images = [
     original: 'https://alliedyacht.s3.amazonaws.com/gallery-image-8-85kB.jpg',
     thumbnail: 'https://alliedyacht.s3.amazonaws.com/gallery-image-8-85kB.jpg',
     originalAlt: 'gallery-image-8',
-    thumbnailAlt: 'gallery-image-8'
+    thumbnailAlt: 'gallery-image-8',
+    loading: 'lazy'
   },
   {
     original: 'https://live.staticflickr.com/65535/51430010012_29b9bb2a31_b.jpg',
     thumbnail: 'https://live.staticflickr.com/65535/51438006839_e64118b989_o.jpg',
     originalAlt: 'gallery-image-1',
-    thumbnailAlt: 'gallery-image-1'
+    thumbnailAlt: 'gallery-image-1',
+    loading: 'lazy'
   },
   {
     original: 'https://live.staticflickr.com/65535/51430770761_52405b928a_b.jpg',
     thumbnail: 'https://live.staticflickr.com/65535/51438224350_bbb9c4f5db_o.jpg',
     originalAlt: 'gallery-image-2',
-    thumbnailAlt: 'gallery-image-2'
+    thumbnailAlt: 'gallery-image-2',
+    loading: 'lazy'
   },
   {
     original: 'https://alliedyacht.s3.amazonaws.com/gallery-image-3-250kB.jpg',
     thumbnail: 'https://alliedyacht.s3.amazonaws.com/gallery-image-3-250kB.jpg',
     originalAlt: 'gallery-image-3',
-    thumbnailAlt: 'gallery-image-3'
+    thumbnailAlt: 'gallery-image-3',
+    loading: 'lazy'
   },
   {
     original: 'https://alliedyacht.s3.amazonaws.com/gallery-image-4-460kB.jpg',
     thumbnail: 'https://alliedyacht.s3.amazonaws.com/gallery-image-4-460kB.jpg',
     originalAlt: 'gallery-image-4',
-    thumbnailAlt: 'gallery-image-4'
+    thumbnailAlt: 'gallery-image-4',
+    loading: 'lazy'
   },
   {
     original: 'https://live.staticflickr.com/65535/51431005458_b09d917564_o.jpg',
     thumbnail: 'https://live.staticflickr.com/65535/51438194770_c0b0c652d0_o.jpg',
     originalAlt: 'gallery-image-5',
-    thumbnailAlt: 'gallery-image-5'
+    thumbnailAlt: 'gallery-image-5',
+    loading: 'lazy'
   },
   {
     original: 'https://live.staticflickr.com/65535/51431005438_68d9c4717e_o.jpg',
     thumbnail: 'https://live.staticflickr.com/65535/51437975454_8549d1c32d_o.jpg',
     originalAlt: 'gallery-image-6',
-    thumbnailAlt: 'gallery-image-6'
+    thumbnailAlt: 'gallery-image-6',
+    loading: 'lazy'
   },
   {
     original: 'https://alliedyacht.s3.amazonaws.com/gallery-image-7-360kB.jpg',
@@ -64,19 +71,22 @@ const images = [
     original: 'https://alliedyacht.s3.amazonaws.com/gallery-image-9-430kb.jpg',
     thumbnail: 'https://alliedyacht.s3.amazonaws.com/gallery-image-9-430kb.jpg',
     originalAlt: 'gallery-image-9',
-    thumbnailAlt: 'gallery-image-9'
+    thumbnailAlt: 'gallery-image-9',
+    loading: 'lazy'
   },
   {
     original: 'https://alliedyacht.s3.amazonaws.com/gallery-image-10-222kB.jpg',
     thumbnail: 'https://alliedyacht.s3.amazonaws.com/gallery-image-10-222kB.jpg',
     originalAlt: 'gallery-image-10',
-    thumbnailAlt: 'gallery-image-10'
+    thumbnailAlt: 'gallery-image-10',
+    loading: 'lazy'
   },
   {
     original: 'https://live.staticflickr.com/65535/51430770766_8a298b092b_o.jpg',
     thumbnail: 'https://live.staticflickr.com/65535/51437485548_9faf6e0875_o.jpg',
     originalAlt: 'gallery-image-11',
-    thumbnailAlt: 'gallery-image-11'
+    thumbnailAlt: 'gallery-image-11',
+    loading: 'lazy'
   }
 ];
 
@@ -90,6 +100,7 @@ const Gallery = () => {
         infinite={true}
         showBullets={true}
         flickThreshold={30}
+        lazyLoad={false}
       />
     </Box>
   );

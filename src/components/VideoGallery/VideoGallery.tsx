1a2bfaed3e4e6da37c14425f66504892e63503c2
Zayats <dmitry.ivanov.iamm@gmail.com>
2024-11-04 07:32:33 +0200

    ZY-15 Fix lint warnings

--- src/components/VideoGallery/VideoGallery.tsx
+++ src/components/VideoGallery/VideoGallery.tsx
@@ -62,7 +62,7 @@ const items = [
 ];
 
 const VideoGallery = () => {
-  const refImg: React.RefObject<HTMLInputElement> = useRef(null);
+  const refImg: any = useRef(null);
   //const customRenderItem = (item, props) => <item.type {...item.props} {...props} />;
 
   const getVideoThumb = (videoId: string) => `https://img.youtube.com/vi/${videoId}/default.jpg`;

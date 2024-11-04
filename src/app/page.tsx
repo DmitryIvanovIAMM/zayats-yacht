76ec201d334cfb9d7448bdbbd59612d7d585b06e
Zayats <dmitry.ivanov.iamm@gmail.com>
2024-11-04 11:15:16 +0200

    ZY-15 Try to use lazy ReactPlayer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,13 @@ import AboutUs from '@/components/AboutUs/AboutUs';
 import Testimonials from '@/components/Testimonials/Testimonials';
 import Gallery from '@/components/Gallery/Gallery';
 import EmptySection from '@/components/EmptySection';
-//import VideoGallery from '@/components/VideoGallery/VideoGallery';
-import dynamic from 'next/dynamic';
+import VideoGallery from '@/components/VideoGallery/VideoGallery';
+//import dynamic from 'next/dynamic';
 //import VideoGallery2 from '@/components/VideoGallery/VideoGallery2';
 
-const VideoGallery = dynamic(() => import('@/components/VideoGallery/VideoGallery'), {
+/*const VideoGallery = dynamic(() => import('@/components/VideoGallery/VideoGallery'), {
   ssr: false
-});
+});*/
 
 export default function Home() {
   return (

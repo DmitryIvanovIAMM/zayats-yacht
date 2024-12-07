7123367aaf56282a8b6a4a617f82397c14e5b664
Zayats <dmitry.ivanov.iamm@gmail.com>
2024-12-07 12:09:47 +0200

    ZY-18 Add connection into PortService.ts

--- src/services/PortService.ts
+++ src/services/PortService.ts
@@ -1,13 +1,20 @@
 import { Port, PortModel } from '@/models/Port';
+import mongoose from 'mongoose';
 
 export default class PortService {
   public getAllPorts = async (): Promise<Port[]> => {
-    const { MONGODB_URI } = process.env;
-    // eslint-disable-next-line no-console
-    console.log('MONGODB_URI: ', MONGODB_URI);
+    try {
+      const { MONGODB_URI } = process.env;
+      const db = await mongoose.connect(MONGODB_URI as string)
+      // eslint-disable-next-line no-console
+      console.log('MONGODB_URI: ', MONGODB_URI);
 
-    return PortModel.find({}).lean<Port[]>();
-  };
+      return PortModel.find({}).lean<Port[]>();
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('Failed to connect and load test data: ', error);
+    }
+  }
 }
 
 export const portService = new PortService();

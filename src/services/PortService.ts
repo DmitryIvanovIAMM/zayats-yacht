48e6e320591c11691032e41f8afd26c30f7ad942
Zayats <dmitry.ivanov.iamm@gmail.com>
2024-12-07 07:18:38 +0200

    ZY-18 Change nothing

--- src/services/PortService.ts
+++ src/services/PortService.ts
@@ -5,6 +5,7 @@ export default class PortService {
     const { MONGODB_URI } = process.env;
     // eslint-disable-next-line no-console
     console.log('MONGODB_URI: ', MONGODB_URI);
+    console.log('MONGODB_URI: ', MONGODB_URI);
 
     return PortModel.find({}).lean<Port[]>();
   };

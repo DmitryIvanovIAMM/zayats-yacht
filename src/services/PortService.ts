9c3f7a776a7358ac43b0abce448da1074e73676a
Zayats <dmitry.ivanov.iamm@gmail.com>
2024-12-07 12:15:20 +0200

    ZY-18 Fix lint warnings

--- src/services/PortService.ts
+++ src/services/PortService.ts
@@ -5,14 +5,17 @@ export default class PortService {
   public getAllPorts = async (): Promise<Port[]> => {
     try {
       const { MONGODB_URI } = process.env;
-      const db = await mongoose.connect(MONGODB_URI as string)
       // eslint-disable-next-line no-console
       console.log('MONGODB_URI: ', MONGODB_URI);
+      await mongoose.connect(MONGODB_URI as string);
+      // eslint-disable-next-line no-console
+      console.log('mongoose connected');
 
       return PortModel.find({}).lean<Port[]>();
     } catch (error) {
       // eslint-disable-next-line no-console
       console.error('Failed to connect and load test data: ', error);
+      return [];
     }
   }
 }

c7c3fe68521e99d4f6f788dca04e80d809d59f8b
Zayats <dmitry.ivanov.iamm@gmail.com>
2024-12-09 10:31:17 +0200

    ZY-18 Small clean up

--- src/services/PortService.ts
+++ src/services/PortService.ts
@@ -1,18 +1,10 @@
 import { Port, PortModel } from '@/models/Port';
-import mongoose from 'mongoose';
 import dbConnect from '@/mongoose/mongoose';
 
 export default class PortService {
   public getAllPorts = async (): Promise<Port[]> => {
     try {
       await dbConnect();
-
-      //const { MONGODB_URI } = process.env;
-      // eslint-disable-next-line no-console
-      //console.log('MONGODB_URI: ', MONGODB_URI);
-      //await mongoose.connect(MONGODB_URI as string);
-      // eslint-disable-next-line no-console
-      console.log('mongoose connected');
 
       return PortModel.find({}).lean<Port[]>();
     } catch (error) {

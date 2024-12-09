901ae8d41d00195e4554203ab59564b576ae6a8d
Zayats <dmitry.ivanov.iamm@gmail.com>
2024-12-09 10:27:05 +0200

    ZY-18 Change way to connect to DB

--- src/services/PortService.ts
+++ src/services/PortService.ts
@@ -1,13 +1,16 @@
 import { Port, PortModel } from '@/models/Port';
 import mongoose from 'mongoose';
+import dbConnect from '@/mongoose/mongoose';
 
 export default class PortService {
   public getAllPorts = async (): Promise<Port[]> => {
     try {
-      const { MONGODB_URI } = process.env;
+      await dbConnect();
+
+      //const { MONGODB_URI } = process.env;
       // eslint-disable-next-line no-console
-      console.log('MONGODB_URI: ', MONGODB_URI);
-      await mongoose.connect(MONGODB_URI as string);
+      //console.log('MONGODB_URI: ', MONGODB_URI);
+      //await mongoose.connect(MONGODB_URI as string);
       // eslint-disable-next-line no-console
       console.log('mongoose connected');
 
@@ -17,7 +20,7 @@ export default class PortService {
       console.error('Failed to connect and load test data: ', error);
       return [];
     }
-  }
+  };
 }
 
 export const portService = new PortService();

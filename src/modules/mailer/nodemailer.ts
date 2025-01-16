b2f3f5137a9da30af97f248b124edd9b6e16589c
Zayats <dmitry.ivanov.iamm@gmail.com>
2025-01-16 11:26:32 +0200

    ZY-25 Small clean up

--- src/modules/mailer/nodemailer.ts
+++ src/modules/mailer/nodemailer.ts
@@ -2,9 +2,6 @@ import * as nodemailer from 'nodemailer';
 import * as aws from 'aws-sdk';
 
 const { ALLIED_AWS_ACCESS_KEY_ID, ALLIED_AWS_SECRET_ACCESS_KEY, ALLIED_AWS_REGION } = process.env;
-console.log('ALLIED_AWS_ACCESS_KEY_ID: ', ALLIED_AWS_ACCESS_KEY_ID);
-console.log('ALLIED_AWS_SECRET_ACCESS KEY: ', ALLIED_AWS_SECRET_ACCESS_KEY);
-console.log('ALLIED_AWS_REGION KEY: ', ALLIED_AWS_REGION);
 
 const awsConfig = {
   accessKeyId: ALLIED_AWS_ACCESS_KEY_ID,

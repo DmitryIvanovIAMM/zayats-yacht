52af1ab1b3b436068a904a789d133d03d33cfad8
Zayats <dmitry.ivanov.iamm@gmail.com>
2025-01-16 11:25:00 +0200

    ZY-25 Add aws access keys

--- src/modules/mailer/nodemailer.ts
+++ src/modules/mailer/nodemailer.ts
@@ -1,11 +1,15 @@
 import * as nodemailer from 'nodemailer';
 import * as aws from 'aws-sdk';
 
-const { AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY, AWS_REGION } = process.env;
+const { ALLIED_AWS_ACCESS_KEY_ID, ALLIED_AWS_SECRET_ACCESS_KEY, ALLIED_AWS_REGION } = process.env;
+console.log('ALLIED_AWS_ACCESS_KEY_ID: ', ALLIED_AWS_ACCESS_KEY_ID);
+console.log('ALLIED_AWS_SECRET_ACCESS KEY: ', ALLIED_AWS_SECRET_ACCESS_KEY);
+console.log('ALLIED_AWS_REGION KEY: ', ALLIED_AWS_REGION);
+
 const awsConfig = {
-  accessKeyId: AWS_ACCESS_KEY,
-  secretAccessKey: AWS_SECRET_ACCESS_KEY,
-  region: AWS_REGION
+  accessKeyId: ALLIED_AWS_ACCESS_KEY_ID,
+  secretAccessKey: ALLIED_AWS_SECRET_ACCESS_KEY,
+  region: ALLIED_AWS_REGION
 };
 
 const transporter = nodemailer.createTransport({

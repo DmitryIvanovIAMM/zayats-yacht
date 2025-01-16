c3e78ec83b334f2d53900196fa22ffd70c103bda
Zayats <dmitry.ivanov.iamm@gmail.com>
2025-01-16 05:40:56 +0200

    ZY-24 Temporary replace logger with console.log

--- src/controllers/EmailController.ts
+++ src/controllers/EmailController.ts
@@ -1,17 +1,21 @@
 'use server';
 
 //import mailTransporter from '../modules/mailer/nodemailer';
-import logger from '../utils/logger';
+//import logger from '../utils/logger';
 import * as quoteRequestUtils from '../utils/quoteRequest';
 import { QuoteRequestForm } from '@/app/quote-request/types';
 import { QuoteRequestModel } from '@/models/QuoteRequest';
 import { Types } from 'mongoose';
 
 export const sendQuoteRequest = async (quoteRequest: QuoteRequestForm) => {
-  logger.info(`sendQuoteRequest().  quoteRequest: ${quoteRequest}`);
+  // eslint-disable-next-line no-console
+  console.log(`sendQuoteRequest().  quoteRequest: ${quoteRequest}`);
+  //logger.info(`sendQuoteRequest().  quoteRequest: ${quoteRequest}`);
 
   const emailMessage = quoteRequestUtils.getQuoteRequestEmail(quoteRequest);
-  logger.info(`emailMessage: ${emailMessage}`);
+  // eslint-disable-next-line no-console
+  console.log(`emailMessage: ${emailMessage}`);
+  //logger.info(`emailMessage: ${emailMessage}`);
 
   const storeQuoteRequest = async (fromEmail: string, message: string) => {
     const newQuoteRequest = new QuoteRequestModel({
@@ -27,20 +31,26 @@ export const sendQuoteRequest = async (quoteRequest: QuoteRequestForm) => {
       console.log('result: ', result);
       return result;
     } catch (error) {
-      logger.error(`Error storing Quote Request in DB: ${error}`);
+      // eslint-disable-next-line no-console
+      console.log(`Error storing Quote Request in DB: ${error}`);
+      //logger.error(`Error storing Quote Request in DB: ${error}`);
       throw error;
     }
   };
 
   try {
     await storeQuoteRequest(quoteRequest.email, emailMessage.text);
-    logger.info(`Sending email message: ${emailMessage}`);
+    // eslint-disable-next-line no-console
+    console.log(`Sending email message: ${emailMessage}`);
+    //logger.info(`Sending email message: ${emailMessage}`);
     //await mailTransporter.sendMail(emailMessage);
     const successResult = { isSuccessful: true, message: 'Email sent successfully' };
     return successResult;
   } catch (err) {
     const errorResult = { isSuccessful: false, message: 'Failed to send Email' };
-    logger.error(`Error sending email: ${err}`);
+    // eslint-disable-next-line no-console
+    console.log(`Error sending email: ${err}`);
+    //logger.error(`Error sending email: ${err}`);
     return errorResult;
   }
 };

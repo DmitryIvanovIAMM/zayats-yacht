44da5bfd714c70091dfc0a80f704e8c56132b0f1
Zayats <dmitry.ivanov.iamm@gmail.com>
2025-01-16 11:32:54 +0200

    ZY-25 Add logging

--- src/modules/mailer/nodemailer.ts
+++ src/modules/mailer/nodemailer.ts
@@ -2,6 +2,12 @@ import * as nodemailer from 'nodemailer';
 import * as aws from 'aws-sdk';
 
 const { ALLIED_AWS_ACCESS_KEY_ID, ALLIED_AWS_SECRET_ACCESS_KEY, ALLIED_AWS_REGION } = process.env;
+// eslint-disable-next-line no-console
+console.log('ALLIED_AWS_ACCESS_KEY_ID: ', ALLIED_AWS_ACCESS_KEY_ID);
+// eslint-disable-next-line no-console
+console.log('ALLIED_AWS_SECRET_ACCESS KEY: ', ALLIED_AWS_SECRET_ACCESS_KEY);
+// eslint-disable-next-line no-console
+console.log('ALLIED_AWS_REGION KEY: ', ALLIED_AWS_REGION);
 
 const awsConfig = {
   accessKeyId: ALLIED_AWS_ACCESS_KEY_ID,

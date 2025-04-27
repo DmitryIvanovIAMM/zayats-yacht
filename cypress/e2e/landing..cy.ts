3360b43671954a433450f3627789ccff414afcdd
Zayats <dmitry.ivanov.iamm@gmail.com>
2025-04-27 08:02:19 +0300

    ZY-33 Add first expectation in cypress test

--- cypress/e2e/landing..cy.ts
+++ cypress/e2e/landing..cy.ts
@@ -1,5 +1,6 @@
-describe('Home Page', () => {
+describe('Landing Page', () => {
   it('displays all 3 products on the home page', () => {
     cy.visit('http://localhost:3000');
+    cy.get('[alt="Allied-Yacht logo"]').should('be.visible');
   });
 });

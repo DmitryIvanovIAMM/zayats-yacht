165d0d9b879271c36f32c8a4c91a2d87a645aa09
Zayats <dmitry.ivanov.iamm@gmail.com>
2025-04-27 08:25:40 +0300

    ZY-33 Add baseUrl in cypress.config.ts

--- cypress/e2e/landing..cy.ts
+++ cypress/e2e/landing..cy.ts
@@ -1,6 +1,6 @@
 describe('Landing Page', () => {
   it('displays all 3 products on the home page', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('/');
     cy.get('[alt="Allied-Yacht logo"]').should('be.visible');
   });
 });

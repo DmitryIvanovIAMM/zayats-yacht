a1a18a26857b0fafdcdd04d42693d1e00975634e
Zayats <dmitry.ivanov.iamm@gmail.com>
2025-05-02 10:07:07 +0300

    AY2-7 Extend the Port model by _id field

--- src/models/Port.ts
+++ src/models/Port.ts
@@ -1,8 +1,10 @@
 import * as mongoose from 'mongoose';
 import { prop, getModelForClass } from '@typegoose/typegoose';
-import { Types } from 'mongoose';
 
 export class Port {
+  @prop({ required: true })
+  _id: mongoose.Types.ObjectId;
+
   @prop({ required: true })
   portName: string;
 
@@ -12,34 +14,6 @@ export class Port {
   @prop({ required: true })
   imageFileName: string;
 }
-// export interface Port extends mongoose.Document {
-//   _id: mongoose.Types.ObjectId;
-//   portName: string;
-//   destinationName: string;
-//   imageFileName: string;
-// }
-//
-// const PortSchema = new mongoose.Schema(
-//   {
-//     portName: {
-//       type: String,
-//       required: true
-//     },
-//     destinationName: {
-//       type: String,
-//       required: true
-//     },
-//     imageFileName: {
-//       type: String,
-//       required: true
-//     }
-//   },
-//   {
-//     timestamps: true
-//   }
-// );
-//
-// export const PortModel = mongoose.models?.ports || mongoose.model<Port>('ports', PortSchema);
 
 export const PortModel =
   mongoose.models?.Port || getModelForClass(Port, { schemaOptions: { timestamps: true } });

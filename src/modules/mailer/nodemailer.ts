e3ba5d353b240f3f9486a1f2899b15455fb471ea
Zayats <dmitry.ivanov.iamm@gmail.com>
2025-05-26 07:53:43 +0300

    AY2-19 Refactor to use same AWS keys by AWS mailer and S3

--- src/modules/mailer/nodemailer.ts
+++ src/modules/mailer/nodemailer.ts
@@ -4,16 +4,16 @@ import { SES, SESClientConfig } from '@aws-sdk/client-ses';
 import { Transporter } from 'nodemailer';
 import SESTransport from 'nodemailer/lib/ses-transport';
 
-const { ALLIED_AWS_ACCESS_KEY_ID, ALLIED_AWS_SECRET_ACCESS_KEY, ALLIED_AWS_REGION } = process.env;
+const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION } = process.env;
 
 // https://github.com/nodemailer/nodemailer/issues/1293
 // Credentials will come from the AWS SDK: https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/loading-node-credentials-environment.html
 const sesClientConfig = {
   apiVersion: '2010-12-01',
-  region: ALLIED_AWS_REGION,
+  region: AWS_REGION,
   credentials: {
-    accessKeyId: ALLIED_AWS_ACCESS_KEY_ID,
-    secretAccessKey: ALLIED_AWS_SECRET_ACCESS_KEY
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY
   }
 } as SESClientConfig;
 export const ses = new SES(sesClientConfig);

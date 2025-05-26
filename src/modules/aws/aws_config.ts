50932b243a689e7c10d60304c0d9e81cde89e726
Zayats <dmitry.ivanov.iamm@gmail.com>
2025-05-26 12:36:41 +0300

    AY2-19 Add AWS_REGION in aws_config

--- src/modules/aws/aws_config.ts
+++ src/modules/aws/aws_config.ts
@@ -1,5 +1,7 @@
+const { AWS_REGION } = process.env;
 // https://github.com/nodemailer/nodemailer/issues/1293
 // Credentials will come from the AWS SDK: https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/loading-node-credentials-environment.html
 export const awsClientConfig = {
-  apiVersion: '2012-10-17'
+  apiVersion: '2012-10-17',
+  region: AWS_REGION
 };

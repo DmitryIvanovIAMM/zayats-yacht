62c13c62e0ca8e170be53e0decf1436cdb88be5b
Zayats <dmitry.ivanov.iamm@gmail.com>
2025-05-30 10:56:24 +0300

    AY2-20 1. Correct import in Table component 2. Revert usage of version of yup

--- src/components/AdminDashboard/AdminPorts/Port/types.ts
+++ src/components/AdminDashboard/AdminPorts/Port/types.ts
@@ -1,5 +1,10 @@
 import { object, string, InferType } from 'yup';
 
+// https://github.com/jquense/yup/issues/1158
+// yup cannot cast optional type, but Maybe type
+// so schema definition should be corrected
+// https://github.com/jquense/yup/issues/1274#issuecomment-879712035
+// use "@hookform/resolvers": "^3.9.1"as it more correctly handles optional types
 export const portSchema = object({
   //_id: string().required('ID is required'),
   portName: string().required('Name is required'),

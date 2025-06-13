c79581e0da6101e684e693aadd67462932a9d872
Zayats <dmitry.ivanov.iamm@gmail.com>
2025-06-13 12:16:37 +0300

    AY2-23 Move also tests from setSailingActivityByAdminStatus.spec to adminManagesSchedules.spec test suite

--- src/app/server-actions/server-actions-tests/adminManagesSchedules.spec.ts
+++ src/app/server-actions/server-actions-tests/adminManagesSchedules.spec.ts
@@ -14,6 +14,7 @@ import {
   addScheduleByAdminAction,
   deleteScheduleByAdminAction,
   getScheduleByAdminAction,
+  setSailingActivityByAdminStatus,
   updateScheduleByAdminAction
 } from '@/app/server-actions/serverActions';
 import { Messages } from '@/helpers/messages';
@@ -450,6 +451,124 @@ describe('admin manages schedules', () => {
       });
       const sailingAfterDelete = await SailingModel.findById(new Types.ObjectId(sailingId));
       expect(sailingAfterDelete.deletedBy).toBeDefined(); // softly delete
+    });
+  });
+
+  describe('setSailingActivityByAdminStatus() action', () => {
+    beforeAll(async () => {
+      await inMemoryDBRunner.reloadTestData();
+    });
+
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('should return error for non-authenticated user', async () => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      mockGetServerSession.mockImplementationOnce(() => null);
+
+      const result = await setSailingActivityByAdminStatus({
+        sailingId: 'some-sailing-id',
+        isActive: true
+      });
+      expect(result).toEqual({
+        success: false,
+        message: Messages.NotAuthenticated
+      });
+    });
+
+    it('should return unauthorized error for non-admin user', async () => {
+      const nonAdminUser = {
+        name: customer2.name,
+        email: customer2.email,
+        image: customer2.role as string
+      };
+      mockGetServerSession.mockImplementationOnce(() => ({
+        user: nonAdminUser,
+        expires: expiresOneHourFromNow
+      }));
+
+      const result = await setSailingActivityByAdminStatus({
+        sailingId: 'some-sailing-id',
+        isActive: true
+      });
+      expect(result).toEqual({
+        success: false,
+        message: Messages.NotAuthorized
+      });
+    });
+
+    it('should return error for wrong sailingUd', async () => {
+      const adminUser = {
+        name: yachtAdmin.name,
+        email: yachtAdmin.email,
+        image: yachtAdmin.role as string
+      };
+      mockGetServerSession.mockImplementationOnce(() => ({
+        user: adminUser,
+        expires: expiresOneHourFromNow
+      }));
+
+      const result = await setSailingActivityByAdminStatus({
+        sailingId: 'some-sailing-id',
+        isActive: true
+      });
+
+      expect(result).toEqual({
+        success: false,
+        message: 'input must be a 24 character hex string, 12 byte Uint8Array, or an integer'
+      });
+    });
+
+    it('should return silently return success non-existed sailingUd', async () => {
+      const adminUser = {
+        name: yachtAdmin.name,
+        email: yachtAdmin.email,
+        image: yachtAdmin.role as string
+      };
+      mockGetServerSession.mockImplementationOnce(() => ({
+        user: adminUser,
+        expires: expiresOneHourFromNow
+      }));
+
+      const result = await setSailingActivityByAdminStatus({
+        sailingId: '60c72b2f9b1e8d001c8e4f5a', // Example non-existing sailing ID
+        isActive: true
+      });
+
+      expect(result).toEqual({
+        success: true,
+        message: Messages.SailingStatusChangedSuccessfully
+      });
+    });
+
+    it('should update sailing activity status by admin', async () => {
+      const sailing = await SailingModel.findById(grandPrixSailingId);
+      expect(sailing.isActive).toBe(true);
+
+      const adminUser = {
+        name: yachtAdmin.name,
+        email: yachtAdmin.email,
+        image: yachtAdmin.role as string
+      };
+      mockGetServerSession.mockImplementationOnce(() => ({
+        user: adminUser,
+        expires: expiresOneHourFromNow
+      }));
+
+      const result = await setSailingActivityByAdminStatus({
+        sailingId: grandPrixSailingId.toString(),
+        isActive: false
+      });
+
+      expect(result).toEqual({
+        success: true,
+        message: Messages.SailingStatusChangedSuccessfully
+      });
+
+      const updatedSailing = await SailingModel.findById(grandPrixSailingId);
+      expect(updatedSailing.isActive).toBe(false);
     });
   });
 });

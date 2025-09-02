b522f08ed628aea8fe8e82f923f5d384c2943bee
Zayats <dmitry.ivanov.iamm@gmail.com>
2025-09-02 12:02:49 +0300

    AY2-27 Change script for api tests

--- src/test-helpers/global-teardown.ts
+++ src/test-helpers/global-teardown.ts
@@ -3,5 +3,5 @@ import 'tsconfig-paths/register';
 import { stopNextServer } from '@/test-helpers/test-next-server';
 
 export default async function teardown() {
-  await stopNextServer(); // 👈 передаём явно
+  await stopNextServer();
 }

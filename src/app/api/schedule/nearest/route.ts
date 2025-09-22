d51db87be856b5df6b07bd9f0a21a2361487ef34
Dmytro Ivanov <dmitry.ivanov.iamm@gmail.com>
2025-09-22 15:31:36 +0300

    ZY2-30 Try to allow localhost origins

--- src/app/api/schedule/nearest/route.ts
+++ src/app/api/schedule/nearest/route.ts
@@ -1,10 +1,37 @@
 import { queryNearestShippingsAction } from '@/app/server-actions/serverActions';
 
-export async function GET() {
+function getAllowedOrigin(request: Request) {
+  const origin = request.headers.get('origin');
+  if (origin && origin.startsWith('http://localhost')) {
+    return origin;
+  }
+  return '';
+}
+
+export async function GET(request: Request) {
   const schedule = await queryNearestShippingsAction(new Date());
+  const allowedOrigin = getAllowedOrigin(request);
 
   return new Response(JSON.stringify(schedule), {
     status: 200,
-    headers: { 'Content-Type': 'application/json' }
+    headers: {
+      'Content-Type': 'application/json',
+      ...(allowedOrigin && { 'Access-Control-Allow-Origin': allowedOrigin }),
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type'
+    }
+  });
+}
+
+export function OPTIONS(request: Request) {
+  const allowedOrigin = getAllowedOrigin(request);
+
+  return new Response(null, {
+    status: 204,
+    headers: {
+      ...(allowedOrigin && { 'Access-Control-Allow-Origin': allowedOrigin }),
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type'
+    }
   });
 }

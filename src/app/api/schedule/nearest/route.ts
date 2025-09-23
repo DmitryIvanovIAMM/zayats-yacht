502d9cd37fe4113aa45d777051c8ee896ed94022
Dmytro Ivanov <dmitry.ivanov.iamm@gmail.com>
2025-09-23 06:03:44 +0300

    ZY2-30 Try to allow any origins

--- src/app/api/schedule/nearest/route.ts
+++ src/app/api/schedule/nearest/route.ts
@@ -29,23 +29,23 @@ export async function GET(request: Request) {
   });
 }
 
-export function OPTIONS(request: Request) {
-  // const allowedOrigin = getAllowedOrigin(request);
-
-  return new Response(null, {
-    status: 204,
-    // headers: {
-    //   ...(allowedOrigin && { 'Access-Control-Allow-Origin': allowedOrigin }),
-    //   'Access-Control-Allow-Methods': 'GET, OPTIONS',
-    //   'Access-Control-Allow-Headers': 'Content-Type'
-    // }
-    headers: {
-      'Content-Type': 'application/json',
-      //...(allowedOrigin && { 'Access-Control-Allow-Origin': allowedOrigin }),
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, OPTIONS, HEAD, PUT, POST, DELETE',
-      'Access-Control-Allow-Headers':
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    }
-  });
-}
+// export function OPTIONS(request: Request) {
+//   // const allowedOrigin = getAllowedOrigin(request);
+//
+//   return new Response(null, {
+//     status: 204,
+//     // headers: {
+//     //   ...(allowedOrigin && { 'Access-Control-Allow-Origin': allowedOrigin }),
+//     //   'Access-Control-Allow-Methods': 'GET, OPTIONS',
+//     //   'Access-Control-Allow-Headers': 'Content-Type'
+//     // }
+//     headers: {
+//       'Content-Type': 'application/json',
+//       //...(allowedOrigin && { 'Access-Control-Allow-Origin': allowedOrigin }),
+//       'Access-Control-Allow-Origin': '*',
+//       'Access-Control-Allow-Methods': 'GET, OPTIONS, HEAD, PUT, POST, DELETE',
+//       'Access-Control-Allow-Headers':
+//         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//     }
+//   });
+// }

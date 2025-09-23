f890bd13bebc635235cdab4131eb5648a0cc4c01
Dmytro Ivanov <dmitry.ivanov.iamm@gmail.com>
2025-09-23 07:20:49 +0300

    ZY2-30 Try to allow any origins

--- src/app/api/schedule/nearest/route.ts
+++ src/app/api/schedule/nearest/route.ts
@@ -35,7 +35,15 @@ export function OPTIONS(request: Request) {
   console.log('OPTIONS allowedOrigin: ', allowedOrigin);
 
   return new Response(null, {
-    status: 200
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      //...(allowedOrigin && { 'Access-Control-Allow-Origin': allowedOrigin }),
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS, HEAD, PUT, POST, DELETE',
+      'Access-Control-Allow-Headers':
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    }
   });
 }
 

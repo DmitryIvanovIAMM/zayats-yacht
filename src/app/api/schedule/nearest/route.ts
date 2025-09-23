8444bed633ecbb422c73821ef916d268a2a94b93
Dmytro Ivanov <dmitry.ivanov.iamm@gmail.com>
2025-09-23 12:15:41 +0300

    ZY2-30 Try to allow any origins

--- src/app/api/schedule/nearest/route.ts
+++ src/app/api/schedule/nearest/route.ts
@@ -31,12 +31,12 @@ export async function GET(request: Request) {
   });
 }
 
-// export function OPTIONS(request: Request) {
-//   return new Response(null, {
-//     status: 200,
-//     headers: {
-//       'Content-Type': 'application/json',
-//       ...getCORSHeaders(request)
-//     }
-//   });
-// }
+export function OPTIONS(request: Request) {
+  return new Response(null, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      ...getCORSHeaders(request)
+    }
+  });
+}

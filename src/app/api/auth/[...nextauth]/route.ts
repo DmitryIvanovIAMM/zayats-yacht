9eafa6018cb439ebeddde2af6e56fe697f26253a
Dmytro Ivanov <dmitry.ivanov.iamm@gmail.com>
2025-09-24 06:06:06 +0300

    ZY2-30 Try to allow any origins

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,12 @@
 import NextAuth from 'next-auth';
 import { authOptions } from './authOptions';
-import { getCORSHeaders } from '@/helpers/cors';
+// import { getCORSHeaders } from '@/helpers/cors';
 
 // CORS preflight handler
 export async function OPTIONS(request: Request) {
-  const headers = getCORSHeaders(request);
-  return new Response(null, { status: 200, headers });
+  // const headers = getCORSHeaders(request);
+  // return new Response(null, { status: 200, headers });
+  return new Response(null, { status: 200, headers: request.headers });
 }
 
 const nextAuthHandler = NextAuth(authOptions);
@@ -13,24 +14,24 @@ const nextAuthHandler = NextAuth(authOptions);
 // Wrap GET and POST to set CORS headers
 export async function GET(request: Request, context: any) {
   const response = await nextAuthHandler(request, context);
-  // eslint-disable-next-line no-console
-  console.log('GET response before CORS headers: ', response);
-  // eslint-disable-next-line no-console
-  console.log('Request headers: ', request.headers);
-  const headers = getCORSHeaders(request);
-  // eslint-disable-next-line no-console
-  console.log('CORS headers to set: ', headers);
-  Object.entries(headers).forEach(([key, value]) => {
-    response.headers.set(key, value as string);
-  });
+  // // eslint-disable-next-line no-console
+  // console.log('GET response before CORS headers: ', response);
+  // // eslint-disable-next-line no-console
+  // console.log('Request headers: ', request.headers);
+  // const headers = getCORSHeaders(request);
+  // // eslint-disable-next-line no-console
+  // console.log('CORS headers to set: ', headers);
+  // Object.entries(headers).forEach(([key, value]) => {
+  //   response.headers.set(key, value as string);
+  // });
   return response;
 }
 
 export async function POST(request: Request, context: any) {
   const response = await nextAuthHandler(request, context);
-  const headers = getCORSHeaders(request);
-  Object.entries(headers).forEach(([key, value]) => {
-    response.headers.set(key, value as string);
-  });
+  // const headers = getCORSHeaders(request);
+  // Object.entries(headers).forEach(([key, value]) => {
+  //   response.headers.set(key, value as string);
+  // });
   return response;
 }

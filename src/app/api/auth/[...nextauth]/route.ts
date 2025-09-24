3384a11af79724c031ba2ae6f6ae72156ca8ff37
Dmytro Ivanov <dmitry.ivanov.iamm@gmail.com>
2025-09-24 06:48:50 +0300

    ZY2-30 Try to allow any origins

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,37 +1,57 @@
 import NextAuth from 'next-auth';
 import { authOptions } from './authOptions';
-// import { getCORSHeaders } from '@/helpers/cors';
+import { getCORSHeaders } from '@/helpers/cors';
 
 // CORS preflight handler
 export async function OPTIONS(request: Request) {
-  // const headers = getCORSHeaders(request);
-  // return new Response(null, { status: 200, headers });
-  return new Response(null, { status: 200, headers: request.headers });
+  const headers = getCORSHeaders(request);
+  return new Response(null, { status: 200, headers });
 }
 
 const nextAuthHandler = NextAuth(authOptions);
 
 // Wrap GET and POST to set CORS headers
 export async function GET(request: Request, context: any) {
+  // eslint-disable-next-line no-console
+  console.log('[nextauth] GET', request.method, request.url);
   const response = await nextAuthHandler(request, context);
-  // // eslint-disable-next-line no-console
-  // console.log('GET response before CORS headers: ', response);
-  // // eslint-disable-next-line no-console
-  // console.log('Request headers: ', request.headers);
-  // const headers = getCORSHeaders(request);
-  // // eslint-disable-next-line no-console
-  // console.log('CORS headers to set: ', headers);
-  // Object.entries(headers).forEach(([key, value]) => {
-  //   response.headers.set(key, value as string);
-  // });
-  return response;
+  // eslint-disable-next-line no-console
+  console.log('[nextauth] GET response.headers', response.headers);
+  const corsHeaders = getCORSHeaders(request);
+  // eslint-disable-next-line no-console
+  console.log('[nextauth] GET CORS headers', corsHeaders);
+  // Merge CORS headers with existing response headers
+  const newHeaders = new Headers(response.headers);
+  Object.entries(corsHeaders).forEach(([key, value]) => {
+    newHeaders.set(key, value as string);
+  });
+  // eslint-disable-next-line no-console
+  console.log('[nextauth] GET newHeaders', newHeaders);
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: newHeaders
+  });
 }
 
 export async function POST(request: Request, context: any) {
+  // eslint-disable-next-line no-console
+  console.log('[nextauth] POST: ', request.method, request.url);
   const response = await nextAuthHandler(request, context);
-  // const headers = getCORSHeaders(request);
-  // Object.entries(headers).forEach(([key, value]) => {
-  //   response.headers.set(key, value as string);
-  // });
-  return response;
+  // eslint-disable-next-line no-console
+  console.log('[nextauth] POST response.heders: ', response.headers);
+  const corsHeaders = getCORSHeaders(request);
+  // eslint-disable-next-line no-console
+  console.log('[nextauth] POST CORS headers: ', corsHeaders);
+  const newHeaders = new Headers(response.headers);
+  Object.entries(corsHeaders).forEach(([key, value]) => {
+    newHeaders.set(key, value as string);
+  });
+  // eslint-disable-next-line no-console
+  console.log('[nextauth] POST newHeaders: ', newHeaders);
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: newHeaders
+  });
 }
